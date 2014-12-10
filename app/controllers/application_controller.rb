2b86ed35110e81623acff44e4176ee91f2726a82
Igor Tchounkovskii <tchounkovskii@gmail.com>
2014-12-10 16:19:05 -0600

    Add data validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :last_name
     devise_parameter_sanitizer.for(:account_update) << :first_name
     devise_parameter_sanitizer.for(:account_update) << :last_name
-
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210145228) do
+ActiveRecord::Schema.define(version: 20141210214914) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
